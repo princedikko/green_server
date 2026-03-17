5e1ae49720e5b23ab8a6890e29396fc75c0facf7
Skill_Point <ceo.skillpoint@gmail.com>
2026-03-17 14:19:07 +0100

    a

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 // Importing the routes
-import router from "./back_end_data_services/server_routes/server_routes.js";
+import jool_routes from "./back_end_data_services/server_routes/server_routes.js";
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.options("*", cors());
 
 app.use(express.json());
 
-app.use("/api/v1/jool", router);
+app.use("/api/v1/jool", jool_routes);
 app.use("*", (req, res) => {
   res.status(404).json("error: route does not exist or invalid");
 });
